e828852e0144e07cfc52ba197f2c2cdd155eba06
sme777 <samson.petrosyan8@gmail.com>
2022-02-10 14:43:20 -0800

    update 2/4 and 2/9 webcasts and assignments

--- src/components/Schedule.js
+++ src/components/Schedule.js
@@ -23,8 +23,8 @@ const Schedule = () => {
                 <ScheduleItem week='2' date='1/26' topic='Nanofabrication with scanning probe' recordingLink='https://www.youtube.com/watch?v=kbFhRH50Fzo' reading=''assignment='Project Presentations'/>
                 <ScheduleItem week='2' date='1/28' topic='Nano-imprint and soft lithography' recordingLink='https://www.youtube.com/watch?v=9jNin_cQgXk&feature=youtu.be' reading =''/>
                 <ScheduleItem week='3' date='2/2' topic='Self-assembly with strong bonds' recordingLink="https://youtu.be/6P49dzeX3pw" reading='' assignment='Paper Presentations'/>
-                <ScheduleItem week='3' date='2/4' topic='Self-assembly with weak bonds' reading=''/>
-                <ScheduleItem week='4' date='2/9' topic='Protein and DNA nanotechnology' reading='' assignment=''/>
+                <ScheduleItem week='3' date='2/4' topic='Self-assembly with weak bonds' recordingLink='https://youtu.be/kjbU9zWzRx4' reading=''/>
+                <ScheduleItem week='4' date='2/9' topic='Protein and DNA nanotechnology' recordingLink='https://youtu.be/OG_g_CMGfyc' reading='' assignment='Paper Presentations'/>
                 <ScheduleItem week='4' date='2/11' topic='Bionanotechnologies' reading=''/>
                 <ScheduleItem week='5' date='2/16' topic='Serial methods' reading='' assignment=''/>
                 <ScheduleItem week='5' date='2/18' topic='Directed Self-Assembly' reading=''/>
